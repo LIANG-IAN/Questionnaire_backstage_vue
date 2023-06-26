a015e740de9bcf73f200c9146bd4dc9d583c558b
ASUS <ms0565119@hotmail.com>
2023-06-27 01:32:20 +0800

    Topic.vue

--- src/views/Topic.vue
+++ src/views/Topic.vue
@@ -3,26 +3,37 @@
 export default {
   data() {
     return {
-      inputType: "text",
-      optionAmount: [1],
-      questionAmount: [1],
+      inputType: ["text"],
+      questionAmount: 1,
     }
   },
   methods: {
     plus(item) {
-      item = item || 0;  // 將 item 初始化為 0，如果它是 undefined
       item++;
       return item;
     },
+    
+    
     minus(item) {
-      item = item || 0;
       item--;
-      if (item <=0){
-        return item = 1; // 避免 input 完全消失
+      if (item <= 0) {
+        return item = 1;
       }
       return item;
+    },
+    
+    
+    // 僅使用在 Watch
+    getChangedIndex(newArray, oldArray) {
+      for (let i = 0; i < newArray.length; i++) {
+        if (newArray[i] !== oldArray[i]) {
+          return i;
+        }
+      }
+      return -1;
     }
-  }
+  },
+  watch: {}
 }
 </script>
 
@@ -35,12 +46,10 @@ export default {
       
       <div class="option-block">
         <label for="answer">回答</label>
-        <span @click="optionAmount[index] = plus(optionAmount[index])">＋</span>
-        <span @click="optionAmount[index] = minus(optionAmount[index])">－</span>
-        <input v-for=" (n,index) in optionAmount[index]" :key="index" type="text">
+        <input type="text">
       </div>
       
-      <select v-model="inputType">
+      <select v-model="inputType[index]">
         <option value="text">
           簡答
         </option>
@@ -53,13 +62,19 @@ export default {
       </select>
     
     </div>
-    <span @click="questionAmount = plus(questionAmount)">＋</span>
-    <span @click="questionAmount = minus(questionAmount)">－</span>
+    <div class="symbol">
+      <span @click="questionAmount = plus(questionAmount)">＋</span>
+      <span @click="questionAmount = minus(questionAmount)">－</span>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .topic {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   
   .question-block {
   
